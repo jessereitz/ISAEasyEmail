f63274811d58fd3101a1e5e659e3648f01d8ba0b
Jesse Reitz <jessereitz1@gmail.com>
2018-10-05 22:40:15 -0500

    Fix all bugs occurring when loading writefree from jsdelivr. Fix all save/load bugs. Update build process to ensure writefree is NOT included.

--- rollupConfig/rollup.config.prod.js
+++ rollupConfig/rollup.config.prod.js
@@ -3,8 +3,12 @@ import babel from 'rollup-plugin-babel';
 import { uglify } from 'rollup-plugin-uglify';
 
 module.exports = {
+  external: ['writefree'],
   input: 'src/index.js',
   output: {
+    globals: {
+      writefree: 'WriteFree',
+    },
     file: 'assets/index.js',
     format: 'iife',
     sourcemap: false,
